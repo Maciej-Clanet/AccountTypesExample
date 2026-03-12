887cf2f0d28d56f48243116c8cfecb70ecb92977
Maciej-Clanet <43215153+Maciej-Clanet@users.noreply.github.com>
2026-03-12 22:20:23 +0000

    auth page

--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -2,44 +2,44 @@ import { useState, useContext, createContext, useEffect } from "react";
 
 const UserContext = createContext();
 
-export default function UserProvider({children}){
+export default function UserProvider({ children }) {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null,
+  );
 
-    const [user,setUser] = useState(
-        JSON.parse(localStorage.getItem("user")) || null
-    )
+  function loginUser(userData) {
+    localStorage.setItem("user", JSON.stringify(userData));
+    setUser(userData);
+  }
+  function logout() {
+    setUser(null);
+    localStorage.removeItem("user");
+  }
 
-    function loginUser(userData){
-        localStorage.setItem("user", JSON.stringify(userData))
-        setUser(userData)
-    }
-    function logout(){
-        setUser(null)
-        localStorage.removeItem("user")
-    }
+  function setTempStudent() {
+    const data = {
+      username: "goober",
+      account_type: "student",
+    };
+    loginUser(data);
+  }
+  function setTempTeacher() {
+    const data = {
+      username: "chad",
+      account_type: "teacher",
+    };
+    loginUser(data);
+  }
 
-    function setTempStudent(){
-        const data = {
-            "username" : "goober",
-            "account_type" : "student"
-        }
-        loginUser(data)
-    }
-    function setTempTeacher(){
-        const data = {
-            "username" : "chad",
-            "account_type": "teacher"
-        }
-        loginUser(data)
-    }
-
-    return(
-        <UserContext.Provider value={{user, loginUser, logout, setTempStudent, setTempTeacher}}>
-            {children}
-        </UserContext.Provider>
-    )
+  return (
+    <UserContext.Provider
+      value={{ user, loginUser, logout, setTempStudent, setTempTeacher }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 }
 
-
-export function useUser(){
-    return useContext(UserContext)
+export function useUser() {
+  return useContext(UserContext);
 }
--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,10 +1,69 @@
-import "./Auth.css"
+import "./Auth.css";
+import { useState } from "react";
+import { useUser } from "../../contexts/UserContext";
 
-export default function Auth(){
+export default function Auth() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [accountType, setAccountType] = useState("student");
 
-    return(
-        <>
-        Auth page
-        </>
-    )
+  const { loginUser } = useUser();
+
+  function handleLogin(e) {
+    e.preventDefault();
+    const data = {
+      username,
+      account_type: accountType,
+    };
+    loginUser(data);
+  }
+
+  return (
+    <div className="auth-page-content">
+      <form className="auth-form" onSubmit={handleLogin}>
+        {/* simple */}
+        <div className="type-options">
+          <label className="type-selector">
+            <h2>Student Account</h2>
+            <p>Enroll and complete courses</p>
+            <input
+              type="radio"
+              name="account_type"
+              value="student"
+              checked={accountType === "student"}
+              onChange={(e) => setAccountType(e.target.value)}
+            />
+          </label>
+          <label className="type-selector">
+            <h2>Teacher Account</h2>
+            <p>Create courses</p>
+            <input
+              type="radio"
+              name="account_type"
+              value="teacher"
+              checked={accountType === "teacher"}
+              onChange={(e) => setAccountType(e.target.value)}
+            />
+          </label>
+        </div>
+        <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
